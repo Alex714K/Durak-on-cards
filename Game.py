b152d19a8130914741d8e882d07c96a9a4a177b3
Alex714k <klimentenko.ad@gmail.com>
2024-01-27 13:11:25 +0300

    Попытался понять приколы с размером экрана, пока что прокол(

--- Game.py
+++ Game.py
@@ -15,9 +15,10 @@ class Game:
 
         self.durak = Durak(Player([], 1), Player([], 2))
 
-        self.size = width, height = 1920, 1080
-        flag = pygame.FULLSCREEN
-        self.display = pygame.display.set_mode(self.size, flag)
+        self.size = width, height = 1650, 950
+        flag = pygame.RESIZABLE
+        self.display = pygame.display.set_mode(self.size)
+        print(pygame.display.Info())
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Durak')
         pygame.display.get_desktop_sizes()
@@ -45,6 +46,8 @@ class Game:
         #     but.position = (but.position[0] - 3, but.position[1])
         # if keys[pygame.K_RIGHT]:
         #     but.position = (but.position[0] + 3, but.position[1])
+        if pygame.mouse.get_pressed()[0]:
+            print(pygame.mouse.get_pos())
         # but.draw(screen)
         #  Вызывает меню, основную игру, настройки
         if self.window == 'menu':
@@ -64,14 +67,14 @@ class Game:
             # 0 but
             self.buttons.append(Button((300, 300), (300, 100), (0, 255, 0), (0, 0, 255), (255, 0, 0)))
             # 1 exit button
-            self.buttons.append(Button((600, 600), (200, 100), (255, 255, 255), (0, 0, 0), (0, 0, 0), text="Exit"))
+            self.buttons.append(Button((1000 - 200, 1000 - 100), (200, 100), (255, 255, 255), (0, 0, 0), (0, 0, 0), text="Exit"))
 
         self.buttons[0].draw(self.display)
         self.buttons[1].draw(self.display)
 
-        pygame.display.flip()
         if self.buttons[0].left_down():
             print_text(self.display, "Loading", (300, 300))
+        pygame.display.flip()
         if self.buttons[1].left_click():
             return False
         return True
