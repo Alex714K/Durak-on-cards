355a50cb42dd10840e35df2dcf590289f26da7ea
Alex714k <klimentenko.ad@gmail.com>
2024-01-31 12:46:01 +0300

    Добавил временнную функцию рисования карты

--- Func.py
+++ Func.py
@@ -1,10 +1,28 @@
 import pygame.surface
 
 
-def print_text(screen: pygame.surface.Surface, message: str, position: tuple):
-    text = font.render(message, True, (0, 0, 0))
-    screen.blit(text, position)
+def print_text(screen: pygame.surface.Surface, message: str, position: tuple, color=(255, 255, 255), centre=False, size=50):
+    font = pygame.font.Font(None, size)
+    text = font.render(message, True, color)
+    if centre:
+        x = position[0] - text.get_width()/2
+        y = position[1] - text.get_height()/2
+        screen.blit(text, (x, y))
+    else:
+        screen.blit(text, position)
+
+
+def draw_card(screen: pygame.surface.Surface, position: tuple, size_rect: tuple, num: str, suit: str, size=30):
+    # suit_txt = font.render(suit, True, (0, 0, 0))
+    font = pygame.font.Font(None, 30)
+    num_txt = font.render(num, True, (0, 0, 0))
+    pygame.draw.rect(screen, (0, 250, 250), (position[0], position[1], size_rect[0], size_rect[1]))
+    if suit in ('spades', 'clubs'):
+        color = (0, 0, 0)
+    else:
+        color = (255, 0, 0)
+    print_text(screen, num, position, (25, 25, 25), size=size)
+    print_text(screen, suit, (position[0], position[1] + num_txt.get_height()), color, size=size)
 
 
 pygame.font.init()
-font = pygame.font.Font(None, 50)
